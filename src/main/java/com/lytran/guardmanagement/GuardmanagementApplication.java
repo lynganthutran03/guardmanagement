0756638ba7b8d2a0904e547722b22aaa088a88e9
Lý Trần Thu Ngân <ngan.lytran.cit22@eiu.edu.vn>
2025-07-07 16:34:33 +0700

    Initial commit

---
+++ src/main/java/com/lytran/guardmanagement/GuardmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.lytran.guardmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuardmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuardmanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lytran/guardmanagement/GuardmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lytran.guardmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuardmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
